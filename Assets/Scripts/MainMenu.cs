3857e3d98da9bf8ece8e135b87d4afb232aa8a9f
cheoljinn <sikdaep0622@gmail.com>
2024-09-02 20:01:54 +0900

    박서연 - 메인 메뉴 추가
    시작 화면으로 쓰일 씬을 추가했습니다

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
